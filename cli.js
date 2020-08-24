575e4ab4fecdef3a3b4675556045b0c3fd6873e3
EmilyCook321 <emilyacook16@gmail.com>
2020-08-24 16:54:30 +0200

    Add password js and passwords/questions file

--- cli.js
+++ cli.js
@@ -1,47 +1,36 @@
-// // function sayHi() {
-// //   console.log("Hello Fwends");
-// // }
-
-const {
-  fetchAsyncQuestionPropertyQuestionProperty,
-} = require("inquirer/lib/utils/utils");
-
-// // setInterval(() => {
-// //   sayHi();
-// // }, 2000);
-
-// const readlineSync = require('readline');
-
-// const interface= readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// })
-
-// interface.question("Please enter your password", password => {
-//     console.log(`Your password is ${password}`);
-//     if (password !== 'abc') {
-//         console.log('Wrong password');
-//         interface.close();
-//         return;
-//     }
-
-//     interface.question("What's your name?", name => {
-//         console.log(`Your name is ${name}`);
-//         interface.close();
-//     })
-// })
-
 const inquirer = require("inquirer");
+const fs = require("fs").promises;
 
 const questions = [
   {
     type: "password",
     name: "password",
-    message: "What's your password?",
+    message: "What's your master password?",
+  },
+  {
+    type: "input",
+    name: "key",
+    message: "Which password do you need?",
+  },
+  {
+    type: "list",
+    name: "task",
+    message: "Which password do you need?",
+    choices: ["set", "get"],
   },
 ];
 
-inquirer.prompt(questions).then((answers) => {
-  console.log(`Your password is ${answers.password}!`);
-  console.log(`Your name is ${answers.name}!`);
+inquirer.prompt(questions).then(async (answers) => {
+  if (answers.password === "123") {
+    console.log("Master Password is correct!");
+    try {
+      const passwordsJSON = await fs.readFile("./passwords.json", "2utf-8");
+      const passwords = JSON.parse(passwordsJSON);
+      console.log(`Your ${answers.key} password is ${passwords[answers.key]}`);
+    } catch (error) {
+      console.error("Something went wrong");
+    }
+  } else {
+    console.log("Master Password is incorrect!");
+  }
 });
