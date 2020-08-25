4e1508de765dc91d14fb8e0a6b5238137cc538c8
EmilyCook321 <emilyacook16@gmail.com>
2020-08-25 14:10:03 +0200

    Add passwords & questions file, edit cli file

--- cli.js
+++ cli.js
@@ -1,6 +1,4 @@
 const inquirer = require("inquirer");
-const fs = require("fs").promises;
-
 const questions = [
   {
     type: "password",
@@ -12,25 +10,49 @@ const questions = [
     name: "key",
     message: "Which password do you need?",
   },
-  {
-    type: "list",
-    name: "task",
-    message: "Which password do you need?",
-    choices: ["set", "get"],
-  },
 ];
 
-inquirer.prompt(questions).then(async (answers) => {
+inquirer.prompt(questions).then((answers) => {
+  console.log(`Your password is the password of ${answers.password}!`);
+
   if (answers.password === "123") {
-    console.log("Master Password is correct!");
-    try {
-      const passwordsJSON = await fs.readFile("./passwords.json", "2utf-8");
-      const passwords = JSON.parse(passwordsJSON);
-      console.log(`Your ${answers.key} password is ${passwords[answers.key]}`);
-    } catch (error) {
-      console.error("Something went wrong");
-    }
+    console.log("Password is correct!");
   } else {
-    console.log("Master Password is incorrect!");
+    console.log("Password is incorrect");
   }
 });
+
+// const {
+//     askStartQuestions,
+//     askGetPasswordQuestions,
+//     askSetPasswordQuestions,
+//     CHOICE_GET,
+//     CHOICE_SET,
+// } = require("./LIB/questions");
+// const { readPassword } = require("./lib/passwords");
+
+// async function main() {
+//     const {masterPassword, action } = await askStartQuestions();
+
+//     if (masterPassword === "123") {
+//         console.log("Master Password is correct");
+//         if (action === CHOICE_GET) {
+//             console.log("Now Get a Password");
+//             const { key } = await askGetPasswordQuestions();
+//             try {
+//                 const password = await readPassword(key);
+//                 console.log(`Your ${key} password is ${password}`);
+//             } cactch (error) {
+//                 console.error("Something went wrong");
+//             }
+//         } else if (action === CHOICE_SET) {
+//             console.log("Now Set a password");
+//             const { key, password } = await askSetPasswordQuestions();
+//             console.log(`New Password: ${key} = ${password}`);
+//         }
+//     } else {
+//         console.log("Master Password is incorrect!");
+//     }
+// }
+
+// main();
