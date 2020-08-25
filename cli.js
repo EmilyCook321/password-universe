9366a72fd272fec69273b942fc2f35b1ce95798d
EmilyCook321 <emilyacook16@gmail.com>
2020-08-25 16:25:37 +0200

    Write passwords

--- cli.js
+++ cli.js
@@ -1,34 +1,35 @@
 const {
-    askStartQuestions,
-    askGetPasswordQuestions,
-    askSetPasswordQuestions,
-    CHOICE_GET,
-    CHOICE_SET,
+  askStartQuestions,
+  askGetPasswordQuestions,
+  askSetPasswordQuestions,
+  CHOICE_GET,
+  CHOICE_SET,
 } = require("./lib/questions");
-const { readPassword } = require("./lib/passwords");
+const { readPassword, writePassword } = require("./lib/passwords");
 
 async function main() {
-    const {masterPassword, action } = await askStartQuestions();
+  const { masterPassword, action } = await askStartQuestions();
 
-    if (masterPassword === "123") {
-        console.log("Master Password is correct");
-        if (action === CHOICE_GET) {
-            console.log("Now Get a Password");
-            const { key } = await askGetPasswordQuestions();
-            try {
-                const password = await readPassword(key);
-                console.log(`Your ${key} password is ${password}`);
-            } catch (error) {
-                console.error("Something went wrong");
-            }
-        } else if (action === CHOICE_SET) {
-            console.log("Now Set a password");
-            const { key, password } = await askSetPasswordQuestions();
-            console.log(`New Password: ${key} = ${password}`);
-        }
-    } else {
-        console.log("Master Password is incorrect!");
+  if (masterPassword === "123") {
+    console.log("Master Password is correct");
+    if (action === CHOICE_GET) {
+      console.log("Now Get a Password");
+      const { key } = await askGetPasswordQuestions();
+      try {
+        const password = await readPassword(key);
+        console.log(`Your ${key} password is ${password}`);
+      } catch (error) {
+        console.error("Something went wrong");
+      }
+    } else if (action === CHOICE_SET) {
+      console.log("Now Set a password");
+      const { key, password } = await askSetPasswordQuestions();
+      await writePassword(key, password);
+      console.log(`New password set`);
     }
+  } else {
+    console.log("Master Password is incorrect!");
+  }
 }
 
 main();
