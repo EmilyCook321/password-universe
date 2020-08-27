730e2510e30abccc20d5abff3c5a3559609203fc
EmilyCook321 <emilyacook16@gmail.com>
2020-08-27 15:01:22 +0200

    Add nodemon to package.json and edit server.js

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-const { express } = require("express");
-const { response } = require("express");
-
 const express = require("express");
+
+const { request, response } = require("express");
 
 const app = express();
 
 const port = 3000;
 
 app.get("/", (request, response) => {
-    console.log("Request /");
-    response.send(equest succeeded);
+  console.log("Request /");
+  response.send("Request succeeded");
 });
 
 app.listen(port, () => {
-    console.log(`Ready! App is listening on http://localhost:${port}`);
+  console.log(`Ready! App is listening on http://localhost:${port}`);
 });
