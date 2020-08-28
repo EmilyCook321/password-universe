a1806b6f1e316bef61cc41b89a8d29c5c3ff2074
EmilyCook321 <emilyacook16@gmail.com>
2020-08-28 09:56:23 +0200

    Add correction to server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { encrypyt } = require("./libcrypto");
 const bodyParser = require("body-parser");
 const { decrypt } = require("./lib/crypto");
 
-const client = new MongoClient (process.env.MONGO_URI);
+const client = new MongoClient(process.env.MONGO_URI);
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,29 +15,29 @@ app.use(bodyParser.json());
 const port = 3000;
 
 async function main() {
-    await client.connect();
-    const database = client.db(process.env.MONGO_DB_NAME);
-    const masterPassword = process.env.MASTER_PASSWORD;
+  await client.connect();
+  const database = client.db(process.env.MONGO_DB_NAME);
+  const masterPassword = process.env.MASTER_PASSWORD;
 
+  app.get("/api/passwords/:name", async (request, response) => {
+    const { name } = request.params;
+    const password = await readPassword(name, database);
+    const decryptedPassword = decrypt(password, masterPassword);
+    response.send(decryptedPassword);
+  });
 
-app.get("/api/passwords/:name", async (request, response) => {
-  const { name } = request.params; 
-  const password = await readPassword(name, database);
-  const decryptedPassword = decrypt(password, masterPassword);
-  response.send(decryptedPassword);
-});
+  app.post("/api/passwords", async (request, response) => {
+    console.log("POST on /api/passwords");
+    const { name, value } = request.body;
+    const encrypytedPassword = encrypyt(value, masterPassword);
+    await writePassword(name, encrypytedPassword, database);
+    response.status(201).send("Password created");
+  });
 
-app.post("/api/passwords", async (request, response) => {
-  console.log("POST on /api/passwords");
-  const { name, value } = request.body;
-  const encrypytedPassword = encrypyt(value, masterPassword);
-  await writePassword(name, encrypytedPassword, database);
-  response.status(201).send("Password created");
-});
-
-app.listen(port, () => {
-  console.log(`Well, hellöchen! App is listening on http://localhost:${port}`);
-});
-
+  app.listen(port, () => {
+    console.log(
+      `Well, hellöchen! App is listening on http://localhost:${port}`
+    );
+  });
 }
-main ();
+main();
