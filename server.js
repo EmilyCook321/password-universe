45dc47cbeef750c76b8b73bf64c80e59d31f7ac2
EmilyCook321 <emilyacook16@gmail.com>
2020-08-31 17:09:37 +0200

    Add passwords router

--- server.js
+++ server.js
@@ -2,67 +2,31 @@ require("dotenv").config();
 
 const express = require("express");
 const { MongoClient } = require("mongodb");
-const { writePassword, readPassword } = require("./lib/passwords");
-const { decrypt, encrypyt } = require("./lib/crypto");
 const bodyParser = require("body-parser");
-const { response } = require("express");
+const createPasswordsRouter = require("./routes/passwords");
 
-const client = new MongoClient(process.env.MONGO_URI, {
+const client = new MongoClient(process.env.MONGO_URL, {
   useUnifiedTopology: true,
 });
 
 const app = express();
-
 app.use(bodyParser.json());
 
 const port = 3000;
-
-// app.use((request, response, next) => {
-//  console.log(`Request ${request.method} on ${request.url}`);
-//  next();
-// });
 
 async function main() {
   await client.connect();
   const database = client.db(process.env.MONGO_DB_NAME);
   const masterPassword = process.env.MASTER_PASSWORD;
 
-  app.use(bodyParse.json());
+
 
   app.use((request, response, next) => {
     console.log(`Request ${request.method} on ${request.url}`);
     next();
   });
 
-
-  app.get("/api/passwords/:name", async (request, response) => {
-    try {
-      const { name } = request.params;
-      const encryptedPassword = await readPassword(name, database);
-      if (!encryptedPassword) {
-        response.status(404).send(`Password ${name} not found`);
-        return;
-      }
-      const password = decrypt(encryptedPassword, masterPassword);
-
-      response.send(password);
-    } catch (error) {
-      console.error(error);
-      response.status(500).send(error.message);
-    }
-  });
-
-  app.post("/api/passwords", async (request, response) => {
-    try {
-      const { name, value } = request.body;
-      const encryptedPassword = encrypt(value, masterPassword);
-      await writePassword(name, encryptedPassword, database);
-      response.status(201).send(`Password ${name} created`);
-    } catch (error) {
-      console.error(error);
-      response.status(500).send(error.message);
-    }
-  });
+  app.use("/api/passwords", createPasswordsRouter(database, masterPassword));
 
   app.get("/", (request, response) => {
     response.sendFile(__dirname + "/index.html");
